2c4989ca0cf076c2ee1f2ae8127183c7fea0cc90
xib-arch <ximple997@gmail.com>
2025-08-25 14:14:50 +0000

    website refresh

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add current year in footer
-document.addEventListener("DOMContentLoaded", () => {
-  const yearEl = document.getElementById('year');
-  if (yearEl) yearEl.textContent = new Date().getFullYear();
-});
